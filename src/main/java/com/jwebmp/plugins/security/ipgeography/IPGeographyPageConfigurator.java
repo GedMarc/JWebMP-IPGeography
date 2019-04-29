6f9071b11c7bd7f26d0dab86cf26304ae314fb62
GedMarc <ged_marc@hotmail.com>
2019-04-29 07:55:11 +0200

    Update for memory leak and security modules separation

---
+++ src/main/java/com/jwebmp/plugins/security/ipgeography/IPGeographyPageConfigurator.java
@@ -0,0 +1,51 @@
+package com.jwebmp.plugins.security.ipgeography;
+
+import com.google.inject.Singleton;
+import com.jwebmp.core.Page;
+import com.jwebmp.core.services.IPageConfigurator;
+
+import javax.validation.constraints.NotNull;
+
+@Singleton
+public class IPGeographyPageConfigurator
+		implements IPageConfigurator<IPGeographyPageConfigurator>
+{
+	/**
+	 * The local storage
+	 */
+	private boolean enabled = true;
+
+	@Override
+	public @NotNull Page<?> configure(Page<?> page)
+	{
+
+		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return enabled;
+	}
+
+
+	/**
+	 * If this page should be rendered with dynamic local storage support
+	 *
+	 * @return
+	 */
+	public boolean isEnabled()
+	{
+		return enabled;
+	}
+
+	/**
+	 * If the page should be rendered with dynamic local storage support. This renders a default page that is then fetched from the server to support cordova applications
+	 *
+	 * @param enabled
+	 */
+	public void setEnabled(boolean enabled)
+	{
+		this.enabled = enabled;
+	}
+}
---
+++ src/main/java/com/jwebmp/plugins/security/ipgeography/implementations/GeoBytesModuleExclusions.java
@@ -0,0 +1,41 @@
+package com.jwebmp.plugins.security.ipgeography.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GeoBytesModuleExclusions
+		implements IGuiceScanModuleExclusions<GeoBytesModuleExclusions>,
+				           IGuiceScanJarExclusions<GeoBytesModuleExclusions>
+{
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-websockets-*");
+		strings.add("jwebmp-plugins-ip-geography-*");
+		strings.add("commons-io-*");
+		strings.add("commons-lang3-*");
+		strings.add("commons-text-*");
+		strings.add("javax.websocket-api-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.websockets");
+		strings.add("com.jwebmp.plugins.security.ipgeography");
+
+		strings.add("org.apache.commons.io");
+		strings.add("org.apache.commons.lang3");
+		strings.add("org.apache.commons.text");
+
+		strings.add("javax.websocket.api");
+		return strings;
+	}
+}
