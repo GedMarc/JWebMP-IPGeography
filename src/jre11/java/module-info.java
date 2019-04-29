b7e6f57b3abc73e37eee1a4cf8ce923008c179d2
GedMarc <ged_marc@hotmail.com>
2019-04-29 07:57:14 +0200

    javax.transaction, javax.activation, Driver,JSR250 merge

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,19 @@
+module com.jwebmp.plugins.security.ipgeography {
+
+	requires com.jwebmp.core;
+	requires java.validation;
+	requires com.google.guice;
+	requires com.fasterxml.jackson.databind;
+	requires com.google.guice.extensions.servlet;
+	requires com.jwebmp.guicedinjection;
+	requires com.jwebmp.interception;
+	requires com.jwebmp.websockets;
+
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.security.ipgeography.IPGeographyPageConfigurator;
+	provides com.jwebmp.websockets.services.IWebSocketAuthDataProvider with com.jwebmp.plugins.security.ipgeography.implementations.GeoBytesDataWSAuth;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.security.ipgeography.implementations.GeoBytesModuleExclusions;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.security.ipgeography.implementations.GeoBytesModuleExclusions;
+
+	opens com.jwebmp.plugins.security.ipgeography to com.google.guice,com.jwebmp.core;
+	opens com.jwebmp.plugins.security.ipgeography.implementations  to com.google.guice,com.jwebmp.core;
+}
