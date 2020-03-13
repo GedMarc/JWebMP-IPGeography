42eedf0606b2b043e658dbebc70f64e79e2865bd
GedMarc <ged_marc@hotmail.com>
2020-03-13 21:42:43 +0200

    CGLib

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -16,7 +16,7 @@ module com.jwebmp.plugins.security.ipgeography {
 	provides com.guicedee.guicedservlets.websockets.services.IWebSocketAuthDataProvider with com.jwebmp.plugins.security.ipgeography.implementations.GeoBytesDataWSAuth;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.security.ipgeography.implementations.GeoBytesModuleExclusions;
 
-	opens com.jwebmp.plugins.security.ipgeography to  com.jwebmp.core, com.fasterxml.jackson.databind;
-	//opens com.jwebmp.plugins.security.ipgeography.services to  com.jwebmp.core, com.fasterxml.jackson.databind;
-	opens com.jwebmp.plugins.security.ipgeography.implementations to  com.jwebmp.core, com.fasterxml.jackson.databind;
+	opens com.jwebmp.plugins.security.ipgeography to com.google.guice, com.jwebmp.core, com.fasterxml.jackson.databind;
+	//opens com.jwebmp.plugins.security.ipgeography.services to com.google.guice, com.jwebmp.core, com.fasterxml.jackson.databind;
+	opens com.jwebmp.plugins.security.ipgeography.implementations to com.google.guice, com.jwebmp.core, com.fasterxml.jackson.databind;
 }
